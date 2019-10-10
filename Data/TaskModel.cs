70ba9b94f051ea1ccf896e81a74a1f97b3e7fd1b
aaltukhov <altukhov.artem.dev@gmail.com>
2019-10-10 22:09:46 +0300

    Adds model classes

---
+++ Data/TaskModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class TaskModel
+    {
+        public int Id { get; private set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CratedDate { get; private set; }
+    }
+}
