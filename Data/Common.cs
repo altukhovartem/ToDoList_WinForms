14048fe9a28be7eee3a52c8bb37ec892b9bc7bef
aaltukhov <altukhov.artem.dev@gmail.com>
2019-10-10 22:09:06 +0300

    Adds connection classes

---
+++ Data/Common.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    class Common
+    {
+        private const string ConnectionString = "conStr";
+        public void AddNewTask()
+        {
+            TaskModel taskModel = new TaskModel
+            {
+                Title = "Task",
+                Description = "TestTask1"
+            };
+
+
+            using (IDbConnection connection = new SqlConnection(GlobalConfig.GetConnectionString(ConnectionString)))
+            {
+                DynamicParameters p = new DynamicParameters();
+                p.Add("@Title", taskModel.Title);
+                p.Add("@Description", taskModel.Description);
+                p.Add("@Description", taskModel.Description);
+                p.Add("@Id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+                int currentId = connection.Execute("[dbo].[TaskModel_Insert]", p, commandType: CommandType.StoredProcedure);
+            }
+            
+        }
+    }
+}
---
+++ Data/GlobalConfig.cs
@@ -0,0 +1,27 @@
+﻿using Data.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public static class GlobalConfig
+    {
+        public static IDataConnection Connection { get; set; }
+        public static string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        public static void InitializeConnection(ConnectionTypes type)
+        {
+            if(type == ConnectionTypes.Dapper)
+            {
+                Connection = new DapperConnection();
+            }
+        }
+    }
+}
